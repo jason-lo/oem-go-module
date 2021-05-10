df651918f39a583113ebf0e96a413c2df2623638
jason-lo <jason.lo@canonical.com>
2021-05-10 11:44:35 +0800

    Update get_tracker_index.go

--- get_tracker_index.go
+++ get_tracker_index.go
@@ -164,15 +164,3 @@ func findItemIndex(aString []string, item string) int {
 	return -1
 
 }
-
-/*
-func main() {
-	//https://docs.google.com/spreadsheets/d/1R8S1fwJTiPTYTqdLu85fZ4kCseUrEsvPl09aoM0AZKU/edit#gid=1623281831
-	spreadsheetId := "1R8S1fwJTiPTYTqdLu85fZ4kCseUrEsvPl09aoM0AZKU"
-	spreadsheetRange := "CMIT/WKS 2021!A2:BF2"
-	indexof := get_index_map(spreadsheetId, spreadsheetRange)
-
-	for k, v := range indexof {
-		fmt.Printf("key[%s] value[%d]\n", k, v)
-	}
-}*/
